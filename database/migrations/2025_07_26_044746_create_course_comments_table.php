8da38023a0c3bc03222fdeeaa7a9a5ec6c493b0f
Indra Yogi <indrayp18@gmail.com>
2025-07-26 13:40:17 +0800

    fix and update table migrations

---
+++ database/migrations/2025_07_26_044746_create_course_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->text('comment');
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('course_comments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_comments');
+    }
+};
