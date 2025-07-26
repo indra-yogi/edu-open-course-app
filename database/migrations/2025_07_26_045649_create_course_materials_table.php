8458c746252a7b08afe06c9b617782475200c72f
Indra Yogi <indrayp18@gmail.com>
2025-07-26 14:47:05 +0800

    fix table name on course materials

--- database/migrations/2025_07_26_045649_create_course_materials_table.php
+++ database/migrations/2025_07_26_045649_create_course_materials_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('courses_materials', function (Blueprint $table) {
+        Schema::create('course_materials', function (Blueprint $table) {
             $table->id();
             $table->foreignId('course_id')->constrained()->onDelete('cascade');
             $table->string('title');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('courses_materials');
+        Schema::dropIfExists('course_materials');
     }
 };
